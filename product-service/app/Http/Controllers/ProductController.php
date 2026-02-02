55a7c31c7661644123a872d264176369f1d4ab58
Ali Fahmi Yusuf <107609258+alifhmiysf@users.noreply.github.com>
2026-02-02 15:36:21 +0700

    feat: init product service with postgresql and CRUD

---
+++ product-service/app/Http/Controllers/ProductController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    // 1. Ambil Semua Produk
+    public function index()
+    {
+        $products = Product::all();
+        return response()->json($products);
+    }
+
+    // 2. Tambah Produk Baru
+    public function store(Request $request)
+    {
+        // Validasi input
+        $request->validate([
+            'name' => 'required',
+            'price' => 'required|numeric',
+            'stock' => 'required|integer'
+        ]);
+
+        // Simpan ke PostgreSQL
+        $product = Product::create($request->all());
+
+        return response()->json([
+            'message' => 'Produk berhasil dibuat!',
+            'data' => $product
+        ], 201);
+    }
+    
+    // 3. Lihat Detail 1 Produk
+    public function show($id)
+    {
+        $product = Product::find($id);
+        
+        if (!$product) {
+            return response()->json(['message' => 'Produk tidak ditemukan'], 404);
+        }
+
+        return response()->json($product);
+    }
+}
---
+++ product-service/routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+// === ROUTE CRUD PRODUK ===
+Route::get('/products', [ProductController::class, 'index']); // Lihat Semua
+Route::post('/products', [ProductController::class, 'store']); // Tambah Baru
+Route::get('/products/{id}', [ProductController::class, 'show']); // Lihat Detail
